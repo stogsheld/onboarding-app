49b7bb60c9afcf02f0240a322e4039250df6a4ab
Oliver Jennings <oliversjennings@gmail.com>
2023-11-15 09:49:40 +0000

    Merged uni laptop & work laptop version

--- src/main/java/capstone/csc8429/onboardingapp/rest/HomeRestController.java
+++ src/main/java/capstone/csc8429/onboardingapp/rest/HomeRestController.java
@@ -1,5 +1,9 @@
 package capstone.csc8429.onboardingapp.rest;
 
+import capstone.csc8429.onboardingapp.entity.User;
+import capstone.csc8429.onboardingapp.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +15,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/home")
 public class HomeRestController {
 
+    private UserService userService;
+
+    public HomeRestController(UserService userService) {
+        this.userService = userService;
+    }
 
     // Access the welcome page
     @GetMapping("/welcome")
     public String homePage(Model theModel) {
 
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        int userId = Integer.parseInt(authentication.getName());
+        User theUser = userService.findById(userId);
+
         theModel.addAttribute("theDate", new java.util.Date());
+        theModel.addAttribute("theUser", theUser);
 
         return "home/welcome";
     }
