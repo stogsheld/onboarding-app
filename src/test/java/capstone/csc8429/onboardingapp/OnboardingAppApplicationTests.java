993cf52a1699d4195c892adf155a639972b3f2c0
Oliver Jennings <94842011+stogsheld@users.noreply.github.com>
2023-11-23 18:13:09 +0000

    Adding Context Configuration

--- src/test/java/capstone/csc8429/onboardingapp/OnboardingAppApplicationTests.java
+++ src/test/java/capstone/csc8429/onboardingapp/OnboardingAppApplicationTests.java
@@ -1,13 +1,15 @@
-//package capstone.csc8429.onboardingapp;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class OnboardingAppApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package capstone.csc8429.onboardingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration
+class OnboardingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
