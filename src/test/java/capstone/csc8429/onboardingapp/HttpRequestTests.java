43808b42d015d0e2990535d6fff85bffc4c46382
Oliver Jennings <94842011+stogsheld@users.noreply.github.com>
2023-11-23 18:35:02 +0000

    Updated Tests

--- src/test/java/capstone/csc8429/onboardingapp/HttpRequestTests.java
+++ src/test/java/capstone/csc8429/onboardingapp/HttpRequestTests.java
@@ -1,26 +1,26 @@
-//package capstone.csc8429.onboardingapp;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.web.client.TestRestTemplate;
-//import org.springframework.boot.test.web.server.LocalServerPort;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//public class HttpRequestTests {
-//
-//    @LocalServerPort
-//    private int port;
-//
-//    @Autowired
-//    private TestRestTemplate restTemplate;
-//
-//    @Test
-//    void homeShouldReturnWelcome() throws Exception {
-//        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/courses/list",
-//                String.class)).contains("Course Name");
-//    }
-//
-//}
+package capstone.csc8429.onboardingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void homeShouldReturnWelcome() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/courses/list",
+                String.class)).contains("Course Name");
+    }
+
+}
--- src/test/java/capstone/csc8429/onboardingapp/SmokeTests.java
+++ src/test/java/capstone/csc8429/onboardingapp/SmokeTests.java
@@ -1,44 +1,44 @@
-//package capstone.csc8429.onboardingapp;
-//
-//import capstone.csc8429.onboardingapp.rest.*;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//// Smoke Tests - Making sure that all REST Controllers exist in the app
-//@SpringBootTest
-//public class SmokeTests {
-//
-//    @Autowired
-//    private AdminRestController adminRestController;
-//    @Test
-//    void adminContextLoads() throws Exception {
-//        assertThat(adminRestController).isNotNull();
-//    }
-//
-//
-//    @Autowired
-//    private CourseRestController courseRestController;
-//    @Test
-//    void courseContextLoads() throws Exception {
-//        assertThat(courseRestController).isNotNull();
-//    }
-//
-//
-//    @Autowired
-//    private HomeRestController homeRestController;
-//    @Test
-//    void homeContextLoads() throws Exception {
-//        assertThat(homeRestController).isNotNull();
-//    }
-//
-//
-//    @Autowired
-//    private LoginRestController loginRestController;
-//    @Test
-//    void loginContextLoads() throws Exception {
-//        assertThat(loginRestController).isNotNull();
-//    }
-//}
+package capstone.csc8429.onboardingapp;
+
+import capstone.csc8429.onboardingapp.rest.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// Smoke Tests - Making sure that all REST Controllers exist in the app
+@SpringBootTest
+public class SmokeTests {
+
+    @Autowired
+    private AdminRestController adminRestController;
+    @Test
+    void adminContextLoads() throws Exception {
+        assertThat(adminRestController).isNotNull();
+    }
+
+
+    @Autowired
+    private CourseRestController courseRestController;
+    @Test
+    void courseContextLoads() throws Exception {
+        assertThat(courseRestController).isNotNull();
+    }
+
+
+    @Autowired
+    private HomeRestController homeRestController;
+    @Test
+    void homeContextLoads() throws Exception {
+        assertThat(homeRestController).isNotNull();
+    }
+
+
+    @Autowired
+    private LoginRestController loginRestController;
+    @Test
+    void loginContextLoads() throws Exception {
+        assertThat(loginRestController).isNotNull();
+    }
+}
